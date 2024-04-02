cc0288bab1c2f4dcbda80c52b7ec31fcbf8b3fe3
Boutsko Dmitry <boutsko@gmail.com>
2024-04-02 22:40:32 +0300

    register serializers in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import ShipOwner, Ship, Port, PortCall, BillOfLading
+from .serializers import ShipOwnerSerializer, ShipSerializer, PortSerializer, PortCallSerializer, BillOfLadingSerializer
 
-# Register your models here.
+class ShipOwnerAdmin(admin.ModelAdmin):
+    pass
+
+class ShipAdmin(admin.ModelAdmin):
+    pass
+
+class PortAdmin(admin.ModelAdmin):
+    pass
+
+class PortCallAdmin(admin.ModelAdmin):
+    pass
+
+class BillOfLadingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ShipOwner, ShipOwnerAdmin)
+admin.site.register(Ship, ShipAdmin)
+admin.site.register(Port, PortAdmin)
+admin.site.register(PortCall, PortCallAdmin)
+admin.site.register(BillOfLading, BillOfLadingAdmin)
