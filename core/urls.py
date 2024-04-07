94d99e388bb2d7a995355961586803d171e83021
Boutsko Dmitry <boutsko@gmail.com>
2024-04-07 03:40:16 +0300

    add pdf generation to bl viewset

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ShipOwnerViewSet, ShipViewSet, PortViewSet, PortCallViewSet, BillOfLadingViewSet
-# from .views import MyPDFView
 from django.urls import path
-from .views import GeneratePDFView
-from .views import generate_example_pdf
 
 
 router = routers.DefaultRouter()
@@ -16,8 +13,7 @@ router.register(r'billofladings', BillOfLadingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('generate-pdf/', GeneratePDFView.as_view(), name='generate_pdf'),
-    path('generate-pdf/<int:bill_of_lading_id>/', GeneratePDFView.as_view(), name='generate_pdf'),  # Specify URL pattern with bill_of_lading_id
-    # path('pdf/', MyPDFView.as_view(), name='pdf_view'),
-    path('example/', generate_example_pdf, name='example_view'),
+    path('billofladings/<int:pk>/generate-pdf/',
+         BillOfLadingViewSet.as_view({'get': 'generate_pdf'}),
+         name='generate_pdf'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from rest_framework import viewsets
 from .models import ShipOwner, Ship, Port, PortCall, BillOfLading
-from .serializers import ShipOwnerSerializer, ShipSerializer, PortSerializer, PortCallSerializer, BillOfLadingSerializer
+from .serializers import (ShipOwnerSerializer,
+                          ShipSerializer,
+                          PortSerializer,
+                          PortCallSerializer,
+                          BillOfLadingSerializer)
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
+from weasyprint import HTML
+
 
 class ShipOwnerViewSet(viewsets.ModelViewSet):
     queryset = ShipOwner.objects.all()
@@ -22,84 +31,26 @@ class BillOfLadingViewSet(viewsets.ModelViewSet):
     queryset = BillOfLading.objects.all()
     serializer_class = BillOfLadingSerializer
 
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from .utilities import generate_bill_of_lading_pdf
-# from rest_framework.response import Response
-# from reportlab.pdfgen import canvas
-# from django.http import HttpResponse
-# from .models import BillOfLading
-#from .serializers import BillOfLadingSerializer
+    def generate_pdf(self, request, pk, *args, **kwargs):
+        bill_of_lading = BillOfLading.objects.get(id=pk)
 
-class GeneratePDFView(APIView):
-    def get(self, request, *args, **kwargs):
-        bill_of_lading_id = kwargs.get('bill_of_lading_id')
-        bill_of_lading = get_object_or_404(BillOfLading, id=bill_of_lading_id)
+        context_data = {
+            'port_call': bill_of_lading.port_call,
+            'shipper': bill_of_lading.shipper,
+            'consignee': bill_of_lading.consignee,
+            'receiver': bill_of_lading.receiver,
+            'notify_party': bill_of_lading.notify_party,
+            'description_of_cargo': bill_of_lading.description_of_cargo,
+            'quantity': bill_of_lading.quantity,
+            'destination': bill_of_lading.destination,
+            'master': bill_of_lading.master,
+        }
 
-        pdf_response = generate_bill_of_lading_pdf(bill_of_lading)
+        template = get_template('bill_of_lading_template.html')
+        html_content = template.render(context_data)
 
-        return pdf_response
+        pdf_file = HTML(string=html_content, base_url=request.build_absolute_uri()).write_pdf()
 
-
-from django.http import HttpResponse
-from django.template.loader import get_template
-from django.views import View
-from reportlab.pdfgen import canvas
-from io import BytesIO
-from .utilities import generate_bill_of_lading_pdf
-
-class MyPDFView(View):
-    def get(self, request, *args, **kwargs):
-        # Get context data to pass to the template
-        context = {'foo': 'bar'}
-
-        # Render the HTML template with context data
-        template = get_template('my_template.html')
-        html_content = template.render(context)
-
-        # Create PDF document using ReportLab
-        pdf_buffer = BytesIO()
-        p = canvas.Canvas(pdf_buffer)
-        p.drawString(100, 750, "My PDF Document")
-
-        # Add HTML content to PDF
-        p.drawString(100, 700, html_content)
-
-        p.showPage()
-        p.save()
-
-        # Prepare HTTP response with PDF content
-        pdf_buffer.seek(0)
-        response = HttpResponse(pdf_buffer, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="my_pdf_document.pdf"'
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="bill_of_lading.pdf"'  
         return response
-
-
-# from django.shortcuts import render
-
-# def example_view(request):
-#     context = {'foo': 'bar'}
-#     return render(request, 'example_template.html', context)
-
-
-
-from django.http import HttpResponse
-from django.template.loader import get_template
-from django.conf import settings
-from weasyprint import HTML
-
-def generate_example_pdf(request):
-    # Get data or context to pass to the template
-    context = {'foo': 'bar'}
-
-    # Render the HTML template with context data
-    template = get_template('example_template.html')
-    html_content = template.render(context)
-
-    # Generate PDF using WeasyPrint
-    pdf_file = HTML(string=html_content, base_url=request.build_absolute_uri()).write_pdf()
-
-    # Create HTTP response with PDF content
-    response = HttpResponse(pdf_file, content_type='application/pdf')
-    response['Content-Disposition'] = 'filename="output.pdf"'
-    return response
